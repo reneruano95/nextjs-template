ccd5f32d8b6076cb4d778246341d328a9d858ec6
Rene Ruano <105190254+reneruano95@users.noreply.github.com>
2024-03-12 12:19:56 +0000

    changes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,10 @@
+import { Metadata } from "next";
 import Link from "next/link";
 import { Container } from "react-bootstrap";
+
+export const metadata: Metadata = {
+    title: 'About',
+};
 
 export default function About() {
     return (
